4b31c70e8952189fdfeaa2a998e64e4d03390090
Igor Boitsev <igor.boitsev@gmail.com>
2020-03-28 20:02:45 +0300

    Переименовал файл

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let money = 25000;
+let income = 'Строительство';
+let addExpenses = 'Квартира, машина, интернет, телефон';
+let deposit = true;
+let mission = 500000;
+let period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
