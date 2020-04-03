9a2c840d3df9360d53849a287e2832a2c199c27d
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-04 00:32:38 +0300

    Выполнил основное задание

--- scripts/script.js
+++ scripts/script.js
@@ -5,34 +5,24 @@ let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
-// Объявление используемых переменных
-let money,
-    income = 'Строительство',
-    addExpenses = [],
-    deposit,
-    mission = 500000,
-    period = 12,
-    budgetDay,
-    expensesAmount = {};
-
+// Объявление используемого объекта
 let appData = {
+  mission : 500000,
   budget : 0,
   budgetDay : 0,
   budgetMonth : 0,
-  expensesMonth : 0
+  expensesMonth : 0,
+  expenses : {}
 }
 
 // Проверка ввода числового значения
-do
-  appData.budget = prompt('Каков ваш месячный доход?');
-while (!isNumber(money));
-
-// addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.');
-// deposit = confirm('Есть ли у вас депозит в банке?');
-
-// Суммарная величина обязательных ваплат за месяц 
-let getExpensesMonth = function() {
-  let sum = 0;
+let start = function() {
+  do
+    appData.budget = prompt('Каков ваш месячный доход?');
+  while (!isNumber(appData.budget));
+}
+// Заполнение статей расхода
+appData.asking = function() {
   let amount = 0;
   let count = +prompt('Сколько у вас статей расхода?');
   for (let i = 1; i <= count; i++) {
@@ -42,34 +32,40 @@ let getExpensesMonth = function() {
     do {
       amount = prompt('Во сколько это обойдется?');
       // Заносим данные в объект
-      expensesAmount['"' + name + '"'] = amount;
+      appData.expenses[name] = amount;
     } while (!isNumber(amount));
-    // Суммируем все расходы
-    sum += +amount;
   }
-  return sum;
 }
-let expensesMonth = getExpensesMonth();
+
+start();
+appData.asking();
+
+// Суммарная величина обязательных ваплат за месяц 
+appData.getExpensesMonth = function() {
+  for(let key in appData.expenses)
+    appData.expensesMonth += +appData.expenses[key];
+  return;
+}
+appData.getExpensesMonth();
 
 // Величина накоплений за месяц
-let getAccumulatedMonth = function() {
-  return (money - expensesMonth);
+appData.getBudget = function() {
+  appData.budgetMonth = appData.budget - appData.expensesMonth;
+  return appData.budgetMonth;
 }
-let accumulatedMonth = getAccumulatedMonth();
-
-// 
-let getTargetMonth = function() {
-  return Math.ceil(mission/accumulatedMonth)
-};
 
 // Бюджет на каждый день
-budgetDay = Math.floor(accumulatedMonth/30);
+appData.budgetDay = Math.floor(appData.getBudget()/30);
+
+appData.getTargetMonth = function() {
+  return Math.ceil(appData.mission/appData.getBudget())
+};
 
 // Определние статуса, уровня дохода
-let getStatusIncome = function() {
-  if (budgetDay > 1200)
+appData.getStatusIncome = function() {
+  if (appData.budgetDay > 1200)
     return ('У вас высокий уровень дохода.');
-      else if (budgetDay > 600) {
+      else if (appData.budgetDay > 600) {
         return ('У вас средний уровень дохода.');
         } else {
               return ('К сложалению, у вас уровень дохода ниже среднего.');
@@ -77,14 +73,20 @@ let getStatusIncome = function() {
 }
 
 // Вывод информации о текущем состоянии
-if (getTargetMonth() > 0) {
-  console.log(getStatusIncome());
-  console.log('Ваши расходы за месяц: ' + expensesMonth);
+if (appData.getTargetMonth() > 0) {
+  console.log(appData.getStatusIncome());
+  console.log('Ваши расходы за месяц: ' + appData.expensesMonth);
   // console.log('Все это уходит на: ' + addExpenses);
-  console.log('Бюджет на день: ' + budgetDay);
-  console.log('Цель будет достигнута за ' + getTargetMonth() + ' месяцев.');
+  console.log('Бюджет на день: ' + appData.budgetDay);
+  console.log('Цель будет достигнута за ' + appData.getTargetMonth() + ' месяцев.');
 } else {
     console.log('Вы за еду работаете? К сожалению, цель не будет достигнута.');
   }
 
-// console.log(expensesAmount);
+// Вывод в консоль всех свойств и значений объекта appData
+console.log('Наша программа включает в себя следующие данные:');
+for(let key in appData) {
+  console.log('Свойство объекта: ' + key + '; его значение: ' + appData[key] + '.');
+}
+
+// console.log(appData);
