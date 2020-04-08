48853938b948ba0f25002732a43775d2febff13b
Igor Boitsev <igor.boitsev@gmail.com>
2020-04-08 18:57:04 +0300

    First commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,57 @@
-// Поле "Месячный доход"
-let salaryAmount = document.querySelector('.salary-amount');
+// // Поле "Месячный доход"
+// let salaryAmount = document.querySelector('.salary-amount');
 
-// Поле "Дополнительный доход (наименование)"
-let extraIncomeTitle = document.querySelector('.extra_income-title');
-// Поле "Дополнительный доход (сумма)"
-let extraIncomeAmount = document.querySelector('.extra_income-amount');
+// // Поле "Дополнительный доход (наименование)"
+// let extraIncomeTitle = document.querySelector('.extra_income-title');
+// // Поле "Дополнительный доход (сумма)"
+// let extraIncomeAmount = document.querySelector('.extra_income-amount');
 
-// Кнопка добавления дополнительного дохода
-let btnIncomeAdd = document.getElementsByTagName('button')[0];
+// // Кнопка добавления дополнительного дохода
+// let btnIncomeAdd = document.getElementsByTagName('button')[0];
 
-// Поле "Возможный доход"
-let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+// // Поле "Возможный доход"
+// let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
 
-// Поле "Обязательные расходы (наименование)"
-let requiredExpensesTitle = document.querySelector('.required_expenses-title');
-// Поле "Обязательные расходы (сумма)"
-let requiredExpensesAmount = document.querySelector('.required_expenses-amount');
+// // Поле "Обязательные расходы (наименование)"
+// let requiredExpensesTitle = document.querySelector('.required_expenses-title');
+// // Поле "Обязательные расходы (сумма)"
+// let requiredExpensesAmount = document.querySelector('.required_expenses-amount');
 
-// Кнопка добавления обязательных расходов
-let btnExpensesAdd = document.getElementsByTagName('button')[1];
+// // Кнопка добавления обязательных расходов
+// let btnExpensesAdd = document.getElementsByTagName('button')[1];
 
-// Поле "Возможные расходы"
-let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+// // Поле "Возможные расходы"
+// let additionalExpensesItem = document.querySelector('.additional_expenses-item');
 
-// Чекбокс для депозита
-let depositCheck = document.getElementById('deposit-check');
+// // Чекбокс для депозита
+// let depositCheck = document.getElementById('deposit-check');
 
-// Поле "Цель"
-let targetAmount = document.querySelector('.target-amount');
+// // Поле "Цель"
+// let targetAmount = document.querySelector('.target-amount');
 
-// Ползунок "Период расчета"
-let periodSelect = document.querySelector('.period-select');
+// // Ползунок "Период расчета"
+// let periodSelect = document.querySelector('.period-select');
 
-// Поле "Доход за месяц"
-let budgetMonthValue = document.getElementsByClassName('result-total')[0];
+// // Поле "Доход за месяц"
+// let budgetMonthValue = document.getElementsByClassName('result-total')[0];
 
-// Поле "Дневной бюджет"
-let budgetDayValue = document.getElementsByClassName('result-total')[1];
+// // Поле "Дневной бюджет"
+// let budgetDayValue = document.getElementsByClassName('result-total')[1];
 
-// Поле "Расход за месяц"
-let expensesMonthValue = document.getElementsByClassName('result-total')[2];
+// // Поле "Расход за месяц"
+// let expensesMonthValue = document.getElementsByClassName('result-total')[2];
 
-// Поле "Возможные доходы"
-let additionalIncomeValue = document.getElementsByClassName('result-total')[3];
+// // Поле "Возможные доходы"
+// let additionalIncomeValue = document.getElementsByClassName('result-total')[3];
 
-// Поле "Возможные расходы"
-let additionalExpensesValue = document.getElementsByClassName('result-total')[4];
+// // Поле "Возможные расходы"
+// let additionalExpensesValue = document.getElementsByClassName('result-total')[4];
 
-// Поле "Накопления за период"
-let incomePeriodValue = document.getElementsByClassName('result-total')[5];
+// // Поле "Накопления за период"
+// let incomePeriodValue = document.getElementsByClassName('result-total')[5];
 
-// Поле "Накопления за период"
-let targetMonthValue = document.getElementsByClassName('result-total')[6];
+// // Поле "Накопления за период"
+// let targetMonthValue = document.getElementsByClassName('result-total')[6];
 
-// Кнопка "Рассчитать"
-let start = document.getElementById('start');
+// // Кнопка "Рассчитать"
+// let start = document.getElementById('start');
